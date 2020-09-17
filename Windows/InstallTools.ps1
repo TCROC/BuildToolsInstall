96d4ef7c2c9b93677b76fbc463cf2c1e7d189d3f
TCROC <travislange12@gmail.com>
2020-09-16 21:41:23 -0400

    Created the ps1 script for installing tools on WIndows

---
+++ Windows/InstallTools.ps1
@@ -0,0 +1,7 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+iex ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1"));
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User");
+choco install -y git.install --params "/GitAndUnixToolsOnPath";
+choco install -y powershell-core;
+choco install -y 7zip.install;

b18e7466474930bec527a8096674dbdf219d2c5b
TCROC <travislange12@gmail.com>
2020-09-17 23:40:08 -0400

    Added git lfs as choco install

--- Windows/InstallTools.ps1
+++ Windows/InstallTools.ps1
@@ -2,6 +2,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force;
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
 iex ((New-Object System.Net.WebClient).DownloadString("https://chocolatey.org/install.ps1"));
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User");
-choco install -y git.install --params "/GitAndUnixToolsOnPath";
+choco install -y git.install --params "/GitAndUnixToolsOnPath /NoGitLfs";
+choco install -y git-lfs.install;
 choco install -y powershell-core;
 choco install -y 7zip.install;

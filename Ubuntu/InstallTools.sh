850fc16913b38df116135aa8a4cc7c44fdaf1297
TCROC <travislange12@gmail.com>
2020-09-21 14:23:39 -0400

    Added commands to install basic toolsets to the linux build agent

--- Ubuntu/InstallTools.sh
+++ Ubuntu/InstallTools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install wget
 sudo apt-get -y install curl
 sudo apt-get -y install git
@@ -9,5 +9,5 @@ sudo apt-get -y install git-lfs
 sudo apt-get -y install p7zip-full
 sudo wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get update
+sudo apt-get -y update
 sudo apt-get -y install powershell

049c5aa0c979b1cbda45e7ed3c55540b2a212943
TCROC <travislange12@gmail.com>
2020-09-21 12:00:26 -0400

    Created an InstallTools.sh for Ubuntu

---
+++ Ubuntu/InstallTools.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get -y install wget
+sudo apt-get -y install curl
+sudo apt-get -y install git
+sudo apt-get -y install git-lfs
+sudo apt-get -y install p7zip-full
+sudo wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get -y install powershell
